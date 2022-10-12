b8c37a2afdf08b074f7e724cad5ab5c8a6fd4b8e
sanket <sanketanilkadam@gmail.com>
2022-10-12 17:34:00 +0530

    refactor apis

--- index.js
+++ index.js
@@ -1,62 +1,87 @@
 import express from "express";
 const app = express();
 import cors from "cors";
+import compression from "compression";
 import { MongoClient } from "mongodb";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-
-
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL = process.env.CONNECTION_URL;
 var database;
-
 
 app.use(
   cors({
     origin: "*",
   })
 );
+app.use(compression({
+  level:9,
+  threshold: 0,
+
+}))
 
 app.get("/api/nifty", async (req, res) => {
-   await database.collection("nifty options").find({}).sort({_id:-1}).limit(1).toArray((err, result) => {
-    try {
-      const data=result[0].datedata;
-      const recent=data[data.length-1];
-      res.send(recent);
-    } catch (err) {}
-  });
- 
+  await database
+    .collection("nifty options")
+    .find({})
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray((err, result) => {
+      try {
+        // const data=result[0].datedata;
+        // const recent=data[data.length-1];
+        res.send(result);
+      } catch (err) {}
+    });
 });
 
 app.get("/api/banknifty", async (req, res) => {
-    await database.collection("bank nifty options").find({}).sort({_id:-1}).limit(1).toArray((err, result) => {
-        try {
-          const data=result[0].datedata;
-          const recent=data[data.length-1];
-          res.send(recent);
-        } catch (err) {}
-      });
+  await database
+    .collection("bank nifty options")
+    .find({})
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray((err, result) => {
+      try {
+        // const data = result[0].datedata;
+        // const recent = data[data.length - 1];
+        res.send(result);
+      } catch (err) {}
+    });
 });
 
 app.get("/api/stocks", async (req, res) => {
-    await database.collection("stocks").find({}).sort({_id:-1}).limit(1).toArray((err, result) => {
-        try {
-          res.send(result);
-        } catch (err) {}
-      });
+  await database
+    .collection("stocks")
+    .find({})
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray((err, result) => {
+      try {
+        res.send(result);
+      } catch (err) {}
+    });
 });
 
 app.get("/api/liveprice", async (req, res) => {
-    await database.collection("index prices").find({}).sort({_id:-1}).limit(1).toArray((err, result) => {
-        try {
-          res.send(result);
-        } catch (err) {}
-      });
+  await database
+    .collection("index prices")
+    .find({})
+    .sort({ _id: -1 })
+    .limit(1)
+    .toArray((err, result) => {
+      try {
+        res.send(result);
+      } catch (err) {}
+    });
 });
 
-
-MongoClient.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+MongoClient.connect(CONNECTION_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  compressors: ["snappy"]
+})
   .then(
     (result) => (database = result.db("test")),
     app.listen(PORT, () =>
